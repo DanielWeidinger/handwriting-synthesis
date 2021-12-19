masking bug

model/training.py
@@ -27,15 +27,11 @@ def get_optimizer(d_model):
     return optimizer
 
 
-def loss_func(label, logits, label_len, eos_loss_obj, coords_loss_obj):
+def loss_func(label, logits, mask, eos_loss_obj, coords_loss_obj):
     coords_label, eos_prob_label = (label[:, :, :2], label[:, :, 2:])
     coords_pred, eos_prob_pred = logits
 
     # End of sentence probability
-    tmp_mask = np.zeros(label.shape[:2])
-    for i, l in enumerate(label_len):
-        tmp_mask[i, :l] = 1
-    mask = tf.convert_to_tensor(tmp_mask, dtype=tf.float32)
     loss_eos = eos_loss_obj(
         eos_prob_label, eos_prob_pred, sample_weight=mask)
 

parse_data.py
@@ -27,7 +27,7 @@ for i, (stroke_fname, c_i, w_id_i) in enumerate(zip(stroke_fnames, transcription
         print(i, '\t', '/', len(stroke_fnames))
 
     x_i = get_stroke_sequence(stroke_fname)
-    valid_mask[i] = ~np.any(np.linalg.norm(x_i[:, :2], axis=1) > 60)
+    # valid_mask[i] = ~np.any(np.linalg.norm(x_i[:, :2], axis=1) > 60)
 
     x[i, :len(x_i), :] = x_i
     x_in[i, :len(x_i)-1, :] = x_i[:-1]
@@ -49,10 +49,10 @@ if not os.path.isdir('data/processed'):
 #     c_one_hot[i, idx, current] = 1
 # c=c_one_hot
 
-np.save('data/processed/x.npy', x[valid_mask])
-np.save('data/processed/x_in.npy', x_in[valid_mask])
-np.save('data/processed/x_out.npy', x_out[valid_mask])
-np.save('data/processed/x_len.npy', x_len[valid_mask])
-np.save('data/processed/c.npy', c[valid_mask])
-np.save('data/processed/c_len.npy', c_len[valid_mask])
-np.save('data/processed/w_id.npy', w_id[valid_mask])
+np.save('data/processed/x.npy', x)
+np.save('data/processed/x_in.npy', x_in)
+np.save('data/processed/x_out.npy', x_out)
+np.save('data/processed/x_len.npy', x_len)
+np.save('data/processed/c.npy', c)
+np.save('data/processed/c_len.npy', c_len)
+np.save('data/processed/w_id.npy', w_id)

train.py
@@ -19,18 +19,22 @@ BATCH_SIZE = 32
 SHUFFLE_BUFFER_SIZE = 100
 TRAIN_SPLIT = 0.8
 
+tar_mask = np.zeros(strokes_in.shape[:2])
+for i, l in enumerate(strokes_len):
+    tar_mask[i, :l] = 1
+
 train_len = int(len(strokes_in)*TRAIN_SPLIT)
 x_train = (chars[train_len:], strokes_in[train_len:])
 y_train = strokes_out[train_len:]
-len_train = strokes_len[train_len:]
+mask_train = tar_mask[train_len:]
 
 x_val = (chars[:train_len], strokes_in[:train_len])
 y_val = strokes_out[:train_len]
-len_val = strokes_len[:train_len]
+mask_val = tar_mask[:train_len]
 
 train_dataset = tf.data.Dataset.from_tensor_slices(
-    (*x_train, y_train, len_train))
-test_dataset = tf.data.Dataset.from_tensor_slices((*x_val, y_val, len_val))
+    (*x_train, y_train, mask_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((*x_val, y_val, mask_val))
 
 train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)  # TODO: use!
@@ -59,8 +63,8 @@ for epoch in range(EPOCHS):
     train_avg_error_distance.reset_states()
 
     # inp -> portuguese, tar -> english
-    for batch, (inp, tar_inp, tar_out, tar_len) in tqdm(enumerate(train_dataset), total=len(train_dataset)):
-        (coords, eos), loss = model.train_step(inp, tar_inp, tar_out, tar_len)
+    for batch, (inp, tar_inp, tar_out, tar_mask) in tqdm(enumerate(train_dataset), total=len(train_dataset)):
+        (coords, eos), loss = model.train_step(inp, tar_inp, tar_out, tar_mask)
         predictions = tf.concat([coords, eos], -1)
         train_loss(loss)
         train_eos_accuracy(eos_accuracy(tar_out, predictions))
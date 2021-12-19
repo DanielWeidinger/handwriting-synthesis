fixed masking bug in loss function

model/training.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 from tensorflow.keras.optimizers.schedules import LearningRateSchedule
 
 
@@ -26,18 +27,21 @@ def get_optimizer(d_model):
     return optimizer
 
 
-def loss_func(label, logits, eos_loss_obj, coords_loss_obj):
+def loss_func(label, logits, label_len, eos_loss_obj, coords_loss_obj):
     coords_label, eos_prob_label = (label[:, :, :2], label[:, :, 2:])
     coords_pred, eos_prob_pred = logits
 
     # End of sentence probability
-    mask = tf.math.logical_not(tf.math.equal(eos_prob_label, 0))
-    mask = tf.cast(mask, dtype=tf.int64)
+    tmp_mask = np.zeros(label.shape[:2])
+    for i, l in enumerate(label_len):
+        tmp_mask[i, :l] = 1
+    mask = tf.convert_to_tensor(tmp_mask, dtype=tf.float32)
     loss_eos = eos_loss_obj(
         eos_prob_label, eos_prob_pred, sample_weight=mask)
 
     # Coordinates (regression therefore MSE)
-    loss_coords = coords_loss_obj(coords_label, coords_pred)
+    loss_coords = coords_loss_obj(
+        coords_label, coords_pred, sample_weight=mask)
 
     return loss_coords, loss_eos
 
@@ -48,7 +52,7 @@ def eos_accuracy(real, pred, threshold=0.8):
     accuracies = tf.equal(real, tf.cast(pred > threshold, dtype=tf.int64))
 
     accuracies = tf.cast(accuracies, dtype=tf.float32)
-    return tf.reduce_sum(accuracies)/accuracies.shape[-2]
+    return tf.reduce_sum(accuracies)/accuracies.shape[1]
 
 
 def avg_error_distance(real, pred):
@@ -58,4 +62,4 @@ def avg_error_distance(real, pred):
     vec_length = tf.math.sqrt(tf.math.add(tf.math.square(
         vectors[:, :, :1]), tf.math.square(vectors[:, :, 1:2])))
 
-    return tf.reduce_sum(vec_length)/vec_length.shape[-2]
+    return tf.reduce_sum(vec_length)/vec_length.shape[1]

model/transformer.py
@@ -76,13 +76,13 @@ class Transformer(Model):
     # batch sizes (the last batch is smaller), use input_signature to specify
     # more generic shapes.
     # (input_signature=[tf.TensorSpec(shape=(None, None), dtype=tf.int64), tf.TensorSpec(shape=(None, None), dtype=tf.int64), tf.TensorSpec(shape=(None, None), dtype=tf.int64)])
-    @tf.function()
-    def train_step(self, inp, tar_inp, tar_out):
+    # @tf.function()
+    def train_step(self, inp, tar_inp, tar_out, tar_len):
         with tf.GradientTape() as tape:
             predictions, _ = self([inp, tar_inp],
                                   training=True)
             eos_loss, coords_loss = loss_func(
-                tar_out, predictions, self.crossentropy, self.mse)
+                tar_out, predictions, tar_len, self.crossentropy, self.mse)
             loss = eos_loss + coords_loss
 
         gradients = tape.gradient(loss, self.trainable_variables)

run.py
@@ -7,9 +7,10 @@ from model.training import avg_error_distance
 # real = tf.constant(
 #     [[[1., 1., 0.], [2., 2., 0.], [3., 3., 1.]]], dtype=tf.float32)
 real = tf.constant(
-    [[[1, 1, 0], [1, 1, 0], [1, 1, 1]]], dtype=tf.float32)
+    [[[1, 1, 0], [1, 1, 0], [1, 1, 1]], [[1, 1, 0], [1, 1, 0], [1, 1, 1]]], dtype=tf.float32)
 pred = tf.constant(
     [[[0., 0., 0.9], [0., 0., 0.7], [0., 0., 0.9]]], dtype=tf.float32)
 
-result = avg_error_distance(real, pred)
-print(result)
+real * [0, 1]
+# result = avg_error_distance(real, pred)
+# print(result)

train.py
@@ -1,9 +1,9 @@
 import tensorflow as tf
 import numpy as np
 import time
-from model.training import avg_error_distance, eos_accuracy
 from tqdm import tqdm
 
+from model.training import avg_error_distance, eos_accuracy
 from model.transformer import Transformer
 
 EPOCHS = 10
@@ -11,6 +11,8 @@ model = Transformer()
 
 strokes_in = np.load('data/processed/x_in.npy')
 strokes_out = np.load('data/processed/x_out.npy')
+# TODO: remove if unnecessary
+strokes_len = np.load('data/processed/x_len.npy')
 chars = np.load('data/processed/c.npy')
 
 BATCH_SIZE = 32
@@ -18,14 +20,17 @@ SHUFFLE_BUFFER_SIZE = 100
 TRAIN_SPLIT = 0.8
 
 train_len = int(len(strokes_in)*TRAIN_SPLIT)
-# TODO: wrong x and y naming, coz that are not the labels
 x_train = (chars[train_len:], strokes_in[train_len:])
 y_train = strokes_out[train_len:]
+len_train = strokes_len[train_len:]
+
 x_val = (chars[:train_len], strokes_in[:train_len])
 y_val = strokes_out[:train_len]
+len_val = strokes_len[:train_len]
 
-train_dataset = tf.data.Dataset.from_tensor_slices((*x_train, y_train))
-test_dataset = tf.data.Dataset.from_tensor_slices((*x_val, y_val))
+train_dataset = tf.data.Dataset.from_tensor_slices(
+    (*x_train, y_train, len_train))
+test_dataset = tf.data.Dataset.from_tensor_slices((*x_val, y_val, len_val))
 
 train_dataset = train_dataset.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE)
 test_dataset = test_dataset.batch(BATCH_SIZE)  # TODO: use!
@@ -54,8 +59,8 @@ for epoch in range(EPOCHS):
     train_avg_error_distance.reset_states()
 
     # inp -> portuguese, tar -> english
-    for batch, (inp, tar_inp, tar_out) in tqdm(enumerate(train_dataset), total=len(train_dataset)):
-        (coords, eos), loss = model.train_step(inp, tar_inp, tar_out)
+    for batch, (inp, tar_inp, tar_out, tar_len) in tqdm(enumerate(train_dataset), total=len(train_dataset)):
+        (coords, eos), loss = model.train_step(inp, tar_inp, tar_out, tar_len)
         predictions = tf.concat([coords, eos], -1)
         train_loss(loss)
         train_eos_accuracy(eos_accuracy(tar_out, predictions))
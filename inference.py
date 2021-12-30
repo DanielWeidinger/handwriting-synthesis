7a3d1980c36d4e5b1fdb44f074446421fc1ca94b
Daniel Weidinger <daniel.weidinger@hp-weidinger.at>
2021-12-30 18:41:13 +0100

    inference

--- inference.py
+++ inference.py
@@ -1,9 +1,45 @@
+import tensorflow_probability as tfp
 import matplotlib.pyplot as plt
 import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
 from data.stroke_utils import MAX_CHAR_LEN, MAX_STROKE_LEN, encode_ascii, offsets_to_coords
 from model.transformer import Transformer
 
 model = Transformer()
+
+
+def sample(inputs, seed):
+    (mixture_weight, stddev1, stddev2, mean1, mean2, correl, end_stroke), _ = model(
+        inputs)
+    # = model.parameterize_distributions(
+    #     outputs)
+
+    # sample for MDN index from mixture weights
+    mixture_dist = tfp.distributions.Categorical(
+        probs=mixture_weight[0, 0])
+    mixture_idx = mixture_dist.sample(seed=seed)
+
+    # retrieve correct distribution values from mixture
+    mean1 = tf.gather(mean1, mixture_idx, axis=-1)
+    mean2 = tf.gather(mean2, mixture_idx, axis=-1)
+    stddev1 = tf.gather(stddev1, mixture_idx, axis=-1)
+    stddev2 = tf.gather(stddev2, mixture_idx, axis=-1)
+    correl = tf.gather(correl, mixture_idx, axis=-1)
+
+    # sample for x, y offsets
+    cov_matrix = [[stddev1 * stddev1, correl * stddev1 * stddev2],
+                  [correl * stddev1 * stddev2, stddev2 * stddev2]]
+    bivariate_gaussian_dist = tfp.distributions.MultivariateNormalDiag(
+        loc=[mean1, mean2], scale_diag=cov_matrix)
+    bivariate_sample = bivariate_gaussian_dist.sample(seed=seed)
+    x, y = bivariate_sample[0, 0], bivariate_sample[1, 1]
+
+    # sample for end of stroke
+    bernoulli = tfp.distributions.Bernoulli(probs=end_stroke)
+    end_cur_stroke = bernoulli.sample(seed=seed)
+    return [x, y, end_cur_stroke]
+
 
 checkpoint_path = "./checkpoints/train"
 ckpt = tf.train.Checkpoint(transformer=model,
@@ -19,17 +55,24 @@ enc_input = [encode_ascii(text)]
 enc_input = tf.keras.preprocessing.sequence.pad_sequences(
     enc_input, maxlen=MAX_CHAR_LEN, padding='post')
 
-start = tf.keras.preprocessing.sequence.pad_sequences(
-    [[[0., 0., 1.]]], maxlen=MAX_STROKE_LEN, padding='post')
+output = np.zeros((1, MAX_STROKE_LEN, 3), dtype=np.float32)
+output[0, 0] = [0, 0, 1]
+output[0, -1] = [0, 0, 1]
 
-(offset, eos), attention_weights = model((enc_input, start))
-output = tf.concat((offset, eos), axis=-1).numpy()
-coords = offsets_to_coords(output[0])
+divisor = 1
+for i in tqdm(tf.range(1, MAX_STROKE_LEN//divisor)):
+    # output = tf.transpose(output_array)  # tf.transpose(output_array.stack())
+    off_x, off_y, eos = sample((enc_input, output), 0)
+    eos = tf.cast(tf.squeeze(eos, axis=-1), dtype=tf.float32)
+    current = tf.concat((off_x, off_y, eos), axis=0)[:, 0]
+    output[0, i] = current
 
 stroke = []
-for x, y, eos in output[0]:
-    stroke.append((x, y))
-    if eos == 1 or True:
+coords = offsets_to_coords(output[0])
+for point in coords:
+    # output = tf.transpose(output_array)  # tf.transpose(output_array.stack())
+    stroke.append((point[0], point[1]))
+    if point[2] == 1:
         coords = list(zip(*stroke))
         plt.plot(coords[0], coords[1], 'k')
         stroke = []
